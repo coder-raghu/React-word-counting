f517d08e02e3fb681d37a104b97b5bc3b7d5c175
Raghu Prajapati <prajapatiraghu32@gmail.com>
2023-12-20 16:43:01 +0530

    Added fixes

--- src/components/WordCount.jsx
+++ src/components/WordCount.jsx
@@ -5,6 +5,7 @@ import toast from 'react-hot-toast';
 
 const WordCount = () => {
     
+    const [text, setText] = useState("")
     const [wordCountTotal, setWordCountTotal] = useState(0)
     const [charTotal, setCharTotal] = useState(0)
     const [charExcludingSpaceTotal, setCharExcludingSpaceTotal] = useState(0)
@@ -12,6 +13,7 @@ const WordCount = () => {
     
     const handleChange = (e) => {
         const text = e.target.value;
+        setText(text)
         setWordCountTotal(text.trim().split(' ').length)
         setCharTotal(text.length)
         setCharExcludingSpaceText(text.replace(/ /g, ""))
@@ -27,21 +29,21 @@ const WordCount = () => {
                 <Col md={12}>
                     <Row className='text-center'>
                         <Col md={4}>
-                            <h4 className='text-primary mb-0 fs-1'>{wordCountTotal}</h4>
+                            <h4 className='text-primary mb-0 fs-1'>{text ? wordCountTotal : "0"}</h4>
                             <div className="">Words</div>
                             <CopyToClipboard text={wordCountTotal} onCopy={() => toast.success(' Text copied.')}>
                             <Button size="sm" variant="secondary" className=''>Copy Text</Button>
                             </CopyToClipboard>
                         </Col>
                         <Col md={4}>
-                            <h4 className='text-primary mb-0 fs-1'>{charTotal}</h4>
+                            <h4 className='text-primary mb-0 fs-1'>{text ? charTotal : "0"}</h4>
                             <div className="">Characters</div>
                             <CopyToClipboard text={wordCountTotal} onCopy={() => toast.success(' Text copied.')}>
                             <Button size="sm" variant="secondary" className=''>Copy Text</Button>
                             </CopyToClipboard>
                         </Col>
                         <Col md={4}>
-                            <h4 className='text-primary mb-0 fs-1'>{charExcludingSpaceTotal}</h4>
+                            <h4 className='text-primary mb-0 fs-1'>{text ? charExcludingSpaceTotal : "0"}</h4>
                             <div className="">Characters excluding spaces</div>
                             <CopyToClipboard text={charExcludingSpaceText} onCopy={() => toast.success(' Text copied.')}>
                             <Button size="sm" variant="secondary" className=''>Copy Text</Button>
